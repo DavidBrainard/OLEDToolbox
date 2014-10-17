74b0d6209f043bcbde7277cb4af1a74883170323
np_cottaris <cottaris@sas.upenn.edu>
2014-10-17 16:51:23 -0400

    Analysis of cloud calibration data

--- AnalyzeCloudCalibrationData.m
+++ AnalyzeCloudCalibrationData.m
@@ -6,7 +6,6 @@ function AnalyzeCloudCalibrationData
     clc
     
     %  Single target runs expect for last 2 runs
-    calibrationFileName = '/Users/Shared/Matlab/Experiments/OLEDExps/PreliminaryData/SamsungOLED_calib.mat';
     calibrationFileName = '/Users/Shared/Matlab/Toolboxes/BrainardLabToolbox/OOCalibrationToolbox/SamsungOLED_CloudsCalib1.mat';
     
     
@@ -40,14 +39,8 @@ function AnalyzeCloudCalibrationData
     
     % Retrieve data
     allCondsData = calibrationDataSet.allCondsData;
-    runParams = calibrationDataSet.runParams;
-    
-    stimParams = runParams.stimParams
-
-    stimulusFileName = runParams.stimulusFileName;
-    load(stimulusFileName);  %loads 'stimParams', 'stimuli'
-    fprintf('Loaded all stimuli');
-    
+    runParams    = calibrationDataSet.runParams;
+    stimParams   = runParams.stimParams;
     conditionsNum = numel(allCondsData);
     
     % Load CIE 1931 CMFs
@@ -91,11 +84,16 @@ function AnalyzeCloudCalibrationData
         
                             luminanceValues(exponentOfOneOverFIndex,oriBiasIndex,frameIndex,patternIndex,targetGrayIndex) = sum(spd'.*vLambda,2);
                             
-                            size(luminanceValues)
+                            if (1==2)
                             demoFrame = runData.demoFrame;
-                            figure(1);
-                            imshow(demoFrame)
+                            h = figure(1);
+                            subplot('Position', [0.02 0.02 0.96 0.96]);
+                            set(h, 'Position', [100 100 1920/2 1080/2], 'Units', 'pixels');
+                            imshow(demoFrame, 'InitialMagnification','fit');
+                            axis 'image'
+                            
                             drawnow;
+                            end
                         end % targetGrayIndex
                     end % patternIndex
                 end % frameIndex
@@ -103,5 +101,59 @@ function AnalyzeCloudCalibrationData
     end % exponentOfOneOVerFIndex
                             
                             
+    figure(2);
+    clf;
+    
+    lum1a = luminanceValues(:,:,:,1,1);
+    lum1b = luminanceValues(:,:,:,2,1);
+   
+    lum2a = luminanceValues(:,:,:,1,1);
+    lum2b = luminanceValues(:,:,:,3,1);
+    
+    lum3a = luminanceValues(:,:,:,1,1);
+    lum3b = luminanceValues(:,:,:,4,1);
+    
+    lum4a = luminanceValues(:,:,:,1,2);
+    lum4b = luminanceValues(:,:,:,2,2);
+    
+    lum5a = luminanceValues(:,:,:,1,2);
+    lum5b = luminanceValues(:,:,:,3,2);
+    
+    lum6a = luminanceValues(:,:,:,1,2);
+    lum6b = luminanceValues(:,:,:,4,2);
+    
+    
+    subplot(2,3,1);
+    plot(lum1a(:), lum1b(:), 'ks');
+    set(gca, 'XLim', [0 500], 'YLim', [0 500]);
+    axis 'square'
+    
+    subplot(2,3,2);
+    plot(lum2a(:), lum2b(:), 'ks');
+    set(gca, 'XLim', [0 500], 'YLim', [0 500]);
+    axis 'square'
+    
+    subplot(2,3,3);
+    plot(lum3a(:), lum3b(:), 'ks');
+    set(gca, 'XLim', [0 500], 'YLim', [0 500]);
+    axis 'square'
+    
+    subplot(2,3,4);
+    plot(lum4a(:), lum4b(:), 'ks');
+    set(gca, 'XLim', [0 500], 'YLim', [0 500]);
+    axis 'square'
+    
+    subplot(2,3,5);
+    plot(lum5a(:), lum5b(:), 'ks');
+    set(gca, 'XLim', [0 500], 'YLim', [0 500]);
+    axis 'square'
+    
+    subplot(2,3,6);
+    plot(lum6a(:), lum6b(:), 'ks');
+    set(gca, 'XLim', [0 500], 'YLim', [0 500]);
+    axis 'square'
+    
+    
+    
     
 end
