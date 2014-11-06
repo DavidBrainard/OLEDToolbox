2e8f35477a87475f5c7a7b279640e611bed2ed6d
Nicolas Cottaris <cottaris@sas.upenn.edu>
2014-11-06 09:35:22 -0500

    Improved analysis, added figs

--- Analysis/PerformLocallySpectralLuminancePrediction.m
+++ Analysis/PerformLocallySpectralLuminancePrediction.m
@@ -1,15 +1,15 @@
 function PerformLocallySpectralLuminancePrediction
 
     % Load stimuli and responses (measured luminances)
-    [stimuli, leftTargetLuminance, rightTargetLuminance, ...
+    [stimuliGammaIn, stimuliGammaOut, leftTargetLuminance, rightTargetLuminance, ...
     trainingIndices, testingIndices] = loadStimuliAndResponses();
 
     % Generate filter bank
-    filterBank = generateFilters(size(stimuli,2), size(stimuli,3));
+    filterBank = generateFilters(size(stimuliGammaIn,2), size(stimuliGammaIn,3));
 
     
     % Construct design matrix
-    conditionsNum = size(stimuli,1);
+    conditionsNum = size(stimuliGammaIn,1);
     filtersNum    = size(filterBank,1);
     featuresNum   = 1 + filtersNum;
     
@@ -19,9 +19,7 @@ function PerformLocallySpectralLuminancePrediction
     XdesignMatrix1 = zeros(conditionsNum, featuresNum);
     XdesignMatrix2 = zeros(conditionsNum, featuresNum);
     
-    % Last element is the bias
-    XdesignMatrix1(:,featuresNum) = 1;
-    XdesignMatrix2(:,featuresNum) = 1;
+    
     
     
     % Check if a parpool is open 
@@ -41,10 +39,12 @@ function PerformLocallySpectralLuminancePrediction
         tic
         filter = double(squeeze(filterBank(filterIndex,:,:)));  
         parfor conditionIndex = 1:conditionsNum
-            stim = double(squeeze(stimuli(conditionIndex,:,:)))/255.0;
-            map = stim .* filter;
-            XdesignMatrix1(conditionIndex, filterIndex) = sum(sum(map));
-            XdesignMatrix2(conditionIndex, filterIndex) = sum(sum(map.^2));
+            stimGammaIn = double(squeeze(stimuliGammaIn(conditionIndex,:,:)))/255.0;
+            stimGammaOut = double(squeeze(stimuliGammaOut(conditionIndex,:,:)))/255.0;
+            gammaInMap  = stimGammaIn  .* filter;
+            gammaOutMap = stimGammaOut .* filter;
+            XdesignMatrix1(conditionIndex, filterIndex) = sum(sum(gammaInMap));
+            XdesignMatrix2(conditionIndex, filterIndex) = sum(sum(gammaOutMap));
         end
         toc
     end
@@ -52,80 +52,179 @@ function PerformLocallySpectralLuminancePrediction
     % ALl done. Delete parallel pool object
     delete(gcp)
 
+    for k = filtersNum:-1:1
+        XdesignMatrix1(:,k+1) = XdesignMatrix1(:,k);
+        XdesignMatrix2(:,k+1) = XdesignMatrix2(:,k);
+    end
+    % First element is the bias
+    XdesignMatrix1(:,1) = 1;
+    XdesignMatrix2(:,1) = 1;
     
-    for featureSpace = 1:2
+    save('intermediate_local_spectral_analysis_gaborSigma30.mat', ...
+        'XdesignMatrix1', 'XdesignMatrix2', ...
+        'trainingIndices', 'testingIndices', ...
+        'leftTargetLuminance', 'rightTargetLuminance');
+    
+    
+    % Add two more design matrices with features = features1.^2
+    XdesignMatrix3 = XdesignMatrix1.^2;
+    XdesignMatrix4 = XdesignMatrix2.^2;
+    
+    for featureSpace = 1:4
         
-        if (featureSpace == 1)
+        if (mod(featureSpace-1,4) == 0)
             X = XdesignMatrix1;
-        else
+        elseif (mod(featureSpace-1,4) == 1)
             X = XdesignMatrix2;
+        elseif (mod(featureSpace-1,4) == 2)
+            X = XdesignMatrix3;
+        elseif (mod(featureSpace-1,4) == 3)
+            X = XdesignMatrix4;
         end
         
-        % check if X is inverible
-        p = inv(X'*X);
         
         Xtrain = X(trainingIndices,:);
         Xtest  = X(testingIndices,:);
         
+        figure(55+featureSpace);
+        imagesc(X)
+        colormap(gray);
+        
+        
         Xdagger = pinv(Xtrain);
+        
+        % check if Xtrain'*Xtrain is inverible
+        
+        fprintf('\n\nRank and size of Xtrain (for feature space: %d):', featureSpace);
+        rank(Xtrain)
+        size(Xtrain)
+        p = inv(Xtrain'*Xtrain);
+        
+        
         weightsVectorLeftTarget  = Xdagger * leftTargetLuminance(trainingIndices);
         weightsVectorRightTarget = Xdagger * rightTargetLuminance(trainingIndices);
         
         % Fit the training data (in-sample)
         fitLeftTargetLuminance  = Xtrain * weightsVectorLeftTarget;
         fitRightTargetLuminance = Xtrain * weightsVectorRightTarget;
-    
+        inSampleError(featureSpace).leftTarget  = sqrt(sum((leftTargetLuminance(trainingIndices)  - fitLeftTargetLuminance).^2)/numel(trainingIndices));
+        inSampleError(featureSpace).rightTarget = sqrt(sum((rightTargetLuminance(trainingIndices) - fitRightTargetLuminance).^2)/numel(trainingIndices));
+        
+        
         % Prediction of test data (out-of-sample)
         predictLeftTargetLuminance  = Xtest * weightsVectorLeftTarget;
         predictRightTargetLuminance = Xtest * weightsVectorRightTarget;
+        outOfSampleError(featureSpace).leftTarget  = sqrt(sum((leftTargetLuminance(testingIndices)  - predictLeftTargetLuminance).^2)/numel(testingIndices));
+        outOfSampleError(featureSpace).rightTarget = sqrt(sum((rightTargetLuminance(testingIndices) - predictRightTargetLuminance).^2)/numel(testingIndices));
+        
+        
+        minAll = min([ ...
+            min(leftTargetLuminance)     min(rightTargetLuminance) ...
+            min(fitLeftTargetLuminance)  min(fitRightTargetLuminance) ...
+            min(predictLeftTargetLuminance) min(predictRightTargetLuminance) ...
+        ]);
     
+        maxAll = max([ ...
+            max(leftTargetLuminance)     max(rightTargetLuminance) ...
+            max(fitLeftTargetLuminance)  max(fitRightTargetLuminance) ...
+            max(predictLeftTargetLuminance) max(predictRightTargetLuminance) ...
+        ]);
+    
+        
         figure(100+featureSpace);
+        clf;
         subplot(2,2,1);
         plot(leftTargetLuminance(trainingIndices), fitLeftTargetLuminance, 'k.');
+        hold on;
+        plot([minAll maxAll], [minAll maxAll], 'r-');
+        hold off
+        set(gca, 'XLim', [minAll maxAll], 'YLim', [minAll maxAll]);
+        axis 'square'
         xlabel('measured luminance');
         ylabel('predicted luminance');
         
         subplot(2,2,2);
         plot(rightTargetLuminance(trainingIndices), fitRightTargetLuminance, 'k.');
+        hold on;
+        plot([minAll maxAll], [minAll maxAll], 'r-');
+        hold off
+        set(gca, 'XLim', [minAll maxAll], 'YLim', [minAll maxAll]);
+        axis 'square'
         xlabel('measured luminance');
         ylabel('predicted luminance');
         
         subplot(2,2,3);
         plot(leftTargetLuminance(testingIndices), predictLeftTargetLuminance, 'k.');
+        hold on;
+        plot([minAll maxAll], [minAll maxAll], 'r-');
+        hold off
+        set(gca, 'XLim', [minAll maxAll], 'YLim', [minAll maxAll]);
+        axis 'square'
         xlabel('measured luminance');
         ylabel('predicted luminance');
         
         subplot(2,2,4);
         plot(rightTargetLuminance(testingIndices), predictRightTargetLuminance, 'k.');
+        hold on;
+        plot([minAll maxAll], [minAll maxAll], 'r-');
+        hold off
+        set(gca, 'XLim', [minAll maxAll], 'YLim', [minAll maxAll]);
+        axis 'square'
         xlabel('measured luminance');
         ylabel('predicted luminance');
         
             
         if (featureSpace == 1)
             subplot(2,2,1);
-            title('RGB settings, Left target (Fit)');
+            title(sprintf('RGB settings-in, Left target (Fit), err = %2.3f', inSampleError(featureSpace).leftTarget));
             
             subplot(2,2,2);
-            title('RGB settings, Right target (Fit)');
+            title(sprintf('RGB settings-in, Right target (Fit), err = %2.3f', inSampleError(featureSpace).rightTarget));
             
             subplot(2,2,3);
-            title('RGB settings, Left target (Prediction)');
+            title(sprintf('RGB settings-in, Left target (Prediction), err = %2.3f', outOfSampleError(featureSpace).leftTarget));
             
             subplot(2,2,4);
-            title('RGB settings, Right target (Prediction)');  
+            title(sprintf('RGB settings-in, Right target (Prediction), err = %2.3f', outOfSampleError(featureSpace).rightTarget));
             drawnow;
-        else
+        elseif (featureSpace == 2)
             subplot(2,2,1);
-            title('RGB settings energy, Left target (Fit)');
+            title(sprintf('RGB settings-out, Left target (Fit), err = %2.3f', inSampleError(featureSpace).leftTarget));
             
             subplot(2,2,2);
-            title('RGB settings energy, Right target (Fit)');
+            title(sprintf('RGB settings-out, Right target (Fit), err = %2.3f', inSampleError(featureSpace).rightTarget));
             
             subplot(2,2,3);
-            title('RGB settings energy, Left target (Prediction)');
+            title(sprintf('RGB settings-out, Left target (Prediction), err = %2.3f', outOfSampleError(featureSpace).leftTarget));
             
             subplot(2,2,4);
-            title('RGB settings energy, Right target (Prediction)');  
+            title(sprintf('RGB settings-out, Right target (Prediction), err = %2.3f', outOfSampleError(featureSpace).rightTarget));
+            drawnow;
+        elseif (featureSpace == 3)
+            subplot(2,2,1);
+            title(sprintf('RGB power-in, Left target (Fit), err = %2.3f', inSampleError(featureSpace).leftTarget));
+            
+            subplot(2,2,2);
+            title(sprintf('RGB power-in, Right target (Fit), err = %2.3f', inSampleError(featureSpace).rightTarget));
+            
+            subplot(2,2,3);
+            title(sprintf('RGB power-in, Left target (Prediction), err = %2.3f', outOfSampleError(featureSpace).leftTarget));
+            
+            subplot(2,2,4);
+            title(sprintf('RGB power-in, Right target (Prediction), err = %2.3f', outOfSampleError(featureSpace).rightTarget)); 
+            drawnow;
+        elseif (featureSpace == 4)
+            subplot(2,2,1);
+            title(sprintf('RGB power-out, Left target (Fit), err = %2.3f', inSampleError(featureSpace).leftTarget));
+            
+            subplot(2,2,2);
+            title(sprintf('RGB power-out, Right target (Fit), err = %2.3f', inSampleError(featureSpace).rightTarget));
+            
+            subplot(2,2,3);
+            title(sprintf('RGB power-out, Left target (Prediction), err = %2.3f', outOfSampleError(featureSpace).leftTarget));
+            
+            subplot(2,2,4);
+            title(sprintf('RGB power-out, Right target (Prediction), err = %2.3f', outOfSampleError(featureSpace).rightTarget)); 
             drawnow;
         end
         
@@ -136,7 +235,7 @@ end
 function filterBank = generateFilters(rowsNum,columnsNum)
 
     % scales
-    gaborSigma = [25 50 100 200];
+    gaborSigma = [30]; % [50]; %50 works ok [25 50 100 200];
     
     totalFiltersNum = 0;
     centers = {};
@@ -188,6 +287,34 @@ function filterBank = generateFilters(rowsNum,columnsNum)
             distanceX = distanceX - envelopeSize(gaborSigmaIndex)/2;
         end
         
+        % Center x-centers
+        xcenters = centers{gaborSigmaIndex}.coords(:,1);
+        xmin = min(xcenters);
+        xmax = max(xcenters);
+        leftBorder = xmin+(columnsNum/2);
+        rightBorder = columnsNum/2 - xmax;
+        if (rightBorder > leftBorder)
+           xcenters = xcenters + ((rightBorder-leftBorder))/2;
+        else
+           xcenters = xcenters - ((leftBorder-rightBorder))/2;
+        end
+        centers{gaborSigmaIndex}.coords(:,1) = xcenters;
+        
+        % Center y-centers
+        ycenters = centers{gaborSigmaIndex}.coords(:,2);
+        ymin = min(ycenters);
+        ymax = max(ycenters);
+        leftBorder = ymin+rowsNum/2;
+        rightBorder = rowsNum/2 - ymax;
+        if (rightBorder > leftBorder)
+           ycenters = ycenters + ((rightBorder-leftBorder))/2;
+        else
+           ycenters = ycenters - (leftBorder-rightBorder)/2;
+        end
+        centers{gaborSigmaIndex}.coords(:,2) = ycenters;
+        
+        
+        
         fprintf('There are %d gabors at scale %d.\n', gaborFilterIndex, gaborSigmaIndex);
         totalFiltersNum = totalFiltersNum + gaborFilterIndex;
     end
@@ -216,15 +343,17 @@ function filterBank = generateFilters(rowsNum,columnsNum)
         for k = 1:size(coords,1)
             xo = coords(k,1);
             yo = coords(k,2);
-            filterIndex = filterIndex + 1;
-            filterBank(filterIndex,:,:) = ...
+            filterKernel = ...
                 single(exp(-0.5*((X-xo)/sigma).^2) .* ...
-                exp(-0.5*((Y-yo)/sigma).^2));
+                       exp(-0.5*((Y-yo)/sigma).^2));
+            % normalize to unit area
+            filterIndex = filterIndex + 1;
+            filterBank(filterIndex,:,:) = filterKernel / sum(filterKernel(:));
             
             if (k == 1)
                 exampleFilter = squeeze(filterBank(filterIndex,:,:));
             else
-                exampleFilter = squeeze(filterBank(filterIndex,:,:)) + exampleFilter;
+                exampleFilter = 0*squeeze(filterBank(filterIndex,:,:)) + exampleFilter;
             end
             
         end
@@ -236,11 +365,11 @@ function filterBank = generateFilters(rowsNum,columnsNum)
 
             
         % Plot coverage
-        for k = 2:size(coords,1)
-           plot(coords(k,1)+xCircle, coords(k,2)+yCircle, 'g-');
-        end
-        plot(coords(1,1)+xCircle, coords(1,2)+yCircle, 'r-');
-        plot(coords(:,1), coords(:,2), 'k+');
+        %for k = 2:size(coords,1)
+        %   plot(coords(k,1)+xCircle, coords(k,2)+yCircle, 'g-');
+        %end
+        %plot(coords(1,1)+xCircle, coords(1,2)+yCircle, 'r-');
+        plot(coords(:,1), coords(:,2), 'r+');
         
         hold off
         set(gca, 'XLim', [-1920/2 1920/2], 'YLim', [-1080/2 1080/2]);
@@ -253,11 +382,18 @@ function filterBank = generateFilters(rowsNum,columnsNum)
 end
 
 
-function [stimuli, leftTargetLuminance, rightTargetLuminance, ...
+function [stimuliGammaIn, stimuliGammaOut, leftTargetLuminance, rightTargetLuminance, ...
     trainingIndices, testingIndices] = loadStimuliAndResponses
     % Load calibration file from /Users1
     calibrationDir  = '/Users1/Shared/Matlab/Experiments/SamsungOLED/PreliminaryData';
     calibrationFile = 'SamsungOLED_CloudsCalib2.mat';
+    gammaFunctionFile = 'GammaFunction.mat';
+    
+    % form gamma function
+    load(sprintf('%s/%s', calibrationDir,gammaFunctionFile))
+    figure(1)
+    plot(gammaFunction.input, gammaFunction.output, 'rs-');
+    drawnow;
     
     calibrationDataSet = loadCalibrationFile(calibrationDir,calibrationFile);
     stimParams = calibrationDataSet.runParams.stimParams;
@@ -278,7 +414,8 @@ function [stimuli, leftTargetLuminance, rightTargetLuminance, ...
     
     % Set up analysis using 1st calibration frame
     stimSize             = size(calibrationDataSet.allCondsData{1,1,1,1,1,1,1}.demoFrame);
-    stimuli              = zeros(stimsNum, stimSize(1), stimSize(2), 'uint8');
+    stimuliGammaIn       = zeros(stimsNum, stimSize(1), stimSize(2), 'uint8');
+    stimuliGammaOut      = stimuliGammaIn;
     leftTargetLuminance  = zeros(stimsNum,1);
     rightTargetLuminance = zeros(stimsNum,1);
     
@@ -300,7 +437,7 @@ function [stimuli, leftTargetLuminance, rightTargetLuminance, ...
         end
         stimIndex = stimIndex + 1;
         
-        if (frameIndex <= framesNum-2)
+        if (mod(frameIndex-1,3 <= 1))
             trainingIndices = [trainingIndices stimIndex];
         else
             testingIndices = [testingIndices stimIndex];
@@ -322,7 +459,27 @@ function [stimuli, leftTargetLuminance, rightTargetLuminance, ...
         spd = SplineSpd(nativeS, (runData.rightSPD)', desiredS);
         rightTargetLuminance(stimIndex) = sum(spd'.*vLambda,2);
         
-        stimuli(stimIndex,:,:) = runData.demoFrame;
+        
+        
+        f = double(runData.demoFrame)/255.0;
+        ff = gammaFunction.output(1+round(f*(numel(gammaFunction.output)-1)));
+        
+        stimuliGammaIn(stimIndex,:,:)  = runData.demoFrame;
+        stimuliGammaOut(stimIndex,:,:) = uint8(ff*255.0);
+        if (1==2)
+            figure(555);
+            subplot(2,1,1)
+            imagesc(squeeze(stimuliGammaIn(stimIndex,:,:)));
+            set(gca, 'CLim', [0 255]);
+            axis 'image'
+            colormap(gray)
+
+            subplot(2,1,2)
+            imagesc(squeeze(stimuliGammaOut(stimIndex,:,:)));
+            set(gca, 'CLim', [0 255]);
+            axis 'image'
+            drawnow
+        end
         
     end
     end
@@ -345,7 +502,7 @@ end
 
 function calibrationDataSet = loadCalibrationFile(calibrationDir,calibrationFile)
     % form full path file
-    fullPathCaFile  = sprintf('%s/%s', calibrationDir,calibrationFile)
+    fullPathCaFile  = sprintf('%s/%s', calibrationDir,calibrationFile);
     
     % create a MAT-file object that supports partial loading and saving.
     matOBJ = matfile(fullPathCaFile, 'Writable', false);
