e75932099c63b2d39bba7e5f67ba2ac5ed72bab6
np_cottaris <cottaris@sas.upenn.edu>
2015-03-20 13:05:35 -0400

    Plotting improvements
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- HDRstuff/BlobbieAnalysis/analyzeDynamicRangeOfBlobbieStimulusSet.m
+++ HDRstuff/BlobbieAnalysis/analyzeDynamicRangeOfBlobbieStimulusSet.m
@@ -16,23 +16,84 @@ function analyzeDynamicRangeOfBlobbieStimulusSet
     lightingConds    = {'area0_front0_ceiling1', 'area1_front0_ceiling0'};
     
     
-    % Generate luminance maps cache filename
-    fullPath = '/Volumes/ColorShare1/Users/Shared/Matlab/Analysis/SamsungProject/AnalyzedData';
-    cacheFilename = fullfile(fullPath,'luminanceMaps.mat');
+
+    % Where to read/write the luminanceMap
+    luminanceMapLocation = 'ColorShare1';
+    %luminanceMapLocation = 'Local';
     
-    % Recompute luminance maps or load existing from cache
+    % Whether to recompute the luminance maps or load existing ones
     reComputeLuminanceMaps = false;
+    
+    
+    if (strcmp(luminanceMapLocation,'ColorShare1'))
+        % Generate luminance maps cache filename
+        fullPath = '/Volumes/ColorShare1/Users/Shared/Matlab/Analysis/SamsungProject/AnalyzedData';
+        cacheFilename = fullfile(fullPath,'luminanceMaps.mat');
+    else
+        cacheFilename = luminanceMaps.mat';
+    end
+    
     if (reComputeLuminanceMaps)
         for lightingCondIndex = 1:numel(lightingConds)
             computeLuminanceMaps(lightingCondIndex);
         end
-        save(cacheFilename, 'luminanceMaps');
+        
+        if (strcmp(luminanceMapLocation,'ColorShare1'))
+            
+            hWaitBar = waitbar(0.3,'Saving to ColorShare. Please wait ...');
+            pause(0.01);
+    
+            % Try to save the computed luminanceMaps to ColorShare1
+            try
+                save(cacheFilename, 'luminanceMaps');
+            catch err
+                close(hWaitBar);
+                delete(hWaitBar);
+                cancelOperation = waitWithDialog('ColorShare1 not mounted?');
+                if (cancelOperation)
+                    return;
+                end
+                fprintf('Trying to save the computed luminanceMaps once again ...\n');
+                save(cacheFilename, 'luminanceMaps');
+            end
+            
+            close(hWaitBar);
+            delete(hWaitBar);
+        else
+            % Save locally
+            save(cacheFilename, 'luminanceMaps');
+        end
     else
         if isempty(luminanceMaps)
+
             fprintf('Fetching %s. Please wait ...', cacheFilename);
-            load(cacheFilename);
+            if (strcmp(luminanceMapLocation,'ColorShare1'))
+                
+                hWaitBar = waitbar(0.3,'Loading from ColorShare. Please wait ...');
+                pause(0.01);
+            
+                 % Try to laod the computed luminanceMaps from ColorShare1
+                try
+                    load(cacheFilename);
+                catch err
+                    close(hWaitBar);
+                    delete(hWaitBar);
+                    cancelOperation = waitWithDialog('ColorShare1 not mounted?');
+                    if (cancelOperation)
+                        return;
+                    end
+                    fprintf('Trying to load the computed luminanceMaps once again ...\n');
+                    load(cacheFilename);
+                end
+                
+                close(hWaitBar);
+                delete(hWaitBar);
+            else
+                % Load locally
+                load(cacheFilename);
+            end
         else
-             fprintf('''luminanceMaps'' is not empty. Will not load again');
+             fprintf('The global variable ''luminanceMaps'' is not empty. Will not re-load any data from the disk.\n');
         end
     end
     
@@ -60,6 +121,21 @@ function analyzeDynamicRangeOfBlobbieStimulusSet
     end
 end
 
+function cancelOperation = waitWithDialog(message)
+
+    % Construct a questdlg with three options
+    tryAgainString = upper('ColorShare1 now mounted. Try again.');
+    choice = questdlg(message, upper(message), tryAgainString ,'Cancel','Cancel');
+    % Handle response
+    switch choice
+        case tryAgainString
+            cancelOperation = false;
+        case 'Cancel'
+            cancelOperation = true;
+    end
+
+end
+
 function generateGUI(lumRange, lightingIndex, dataType)
 
     global shapeConds
@@ -82,24 +158,24 @@ function generateGUI(lumRange, lightingIndex, dataType)
     if (strcmp(dataType, 'image'))
         hFig = figure(lightingIndex);
         clf;
-        set(hFig, 'MenuBar','None', 'Position', [10+20*lightingIndex, 10+20*lightingIndex, 1845, 770]);
+        set(hFig, 'MenuBar','None', 'Position', [10, 10+700*(lightingIndex-1), 1845, 770]);
         set(hFig, 'Name', sprintf('Lighting: %s; Lum range: %2.2f - %2.2f', lightingConds{lightingIndex}, lumRange(1), lumRange(2)));
         % generate new menu
         menuHandle = uimenu(hFig,'Label','Display options'); 
 
         frh = uimenu(menuHandle ,'Label','Luminance range ...');                 
-        uimenu(frh,'Label','Full range',            'Callback', {@fullLumRangeCallBackFunction, lightingIndex});
-        uimenu(frh,'Label','Percentile (99)',       'Callback', {@percentileLumRangeCallBackFunction, 99, lightingIndex});
-        uimenu(frh,'Label','Percentile (98)',       'Callback', {@percentileLumRangeCallBackFunction, 98, lightingIndex});
-        uimenu(frh,'Label','Percentile (95)',       'Callback', {@percentileLumRangeCallBackFunction, 95, lightingIndex});
-        uimenu(frh,'Label','Percentile (90)',       'Callback', {@percentileLumRangeCallBackFunction, 90, lightingIndex});
-        uimenu(frh,'Label','Percentile (80)',       'Callback', {@percentileLumRangeCallBackFunction, 80, lightingIndex});
-        uimenu(frh,'Label','Percentile (70)',       'Callback', {@percentileLumRangeCallBackFunction, 70, lightingIndex});
-        uimenu(frh,'Label','Percentile (50)',       'Callback', {@percentileLumRangeCallBackFunction, 50, lightingIndex});
+        uimenu(frh,'Label','Full range',    'Callback', {@fullLumRangeCallBackFunction, lightingIndex});
+        uimenu(frh,'Label','Clip top 1%',   'Callback', {@percentileLumRangeCallBackFunction, 99, lightingIndex});
+        uimenu(frh,'Label','Clip top 2%',   'Callback', {@percentileLumRangeCallBackFunction, 98, lightingIndex});
+        uimenu(frh,'Label','Clip top 5%',   'Callback', {@percentileLumRangeCallBackFunction, 95, lightingIndex});
+        uimenu(frh,'Label','Clip top 10%',  'Callback', {@percentileLumRangeCallBackFunction, 90, lightingIndex});
+        uimenu(frh,'Label','Clip top 20%',  'Callback', {@percentileLumRangeCallBackFunction, 80, lightingIndex});
+        uimenu(frh,'Label','Clip top 30%',  'Callback', {@percentileLumRangeCallBackFunction, 70, lightingIndex});
+        uimenu(frh,'Label','Clip top 50%',  'Callback', {@percentileLumRangeCallBackFunction, 50, lightingIndex});
         
         frh = uimenu(menuHandle ,'Label','Data cursor ...');   
-        uimenu(frh,'Label','ON', 'Callback', {@dataCursorCallBackFunction, true, hFig});
-        uimenu(frh,'Label','OFF', 'Callback', {@dataCursorCallBackFunction, false, hFig});
+        uimenu(frh,'Label','ON', 'Callback',  {@dataCursorCallBackFunction, true, hFig, 'image'});
+        uimenu(frh,'Label','OFF', 'Callback', {@dataCursorCallBackFunction, false, hFig, 'image'});
         
         % Generate a tab group
         tabGroupHandle = uitabgroup('Parent', hFig);
@@ -127,7 +203,8 @@ function generateGUI(lumRange, lightingIndex, dataType)
                     imshow(lumMap);
                     set(gca, 'CLim', lumRange);
                     cRatio = max(lumMap(:))/min(lumMap(:));
-                    text(620,70, sprintf('LR=%2.1f', cRatio), 'FontSize', 14, 'FontName', 'system', 'Color',  [0.2 0.99 0.8]);
+                    text(0.77,0.9,sprintf('LR=%2.1f', cRatio),'Units','normalized', 'FontSize', 14, 'FontName', 'system', 'Color',  [0.2 0.99 0.8]);
+                    %text(620,70, sprintf('LR=%2.1f', cRatio), 'FontSize', 14, 'FontName', 'system', 'Color',  [0.2 0.99 0.8]);
                     axis 'image'
                     drawnow;
                 end
@@ -151,24 +228,34 @@ function generateGUI(lumRange, lightingIndex, dataType)
         
         hFig = figure(10+lightingIndex);
         clf;
-        set(hFig, 'MenuBar','None', 'Position', [50+20*lightingIndex, 50+20*lightingIndex, 1845, 770]);
+        set(hFig, 'MenuBar','None', 'Position', [700, 10+700*(lightingIndex-1), 1845, 770]);
         set(hFig, 'Name', sprintf('Lighting: %s; Lum range: %2.2f - %2.2f', lightingConds{lightingIndex}, lumRange(1), lumRange(2)));
         
         % generate new menu
         menuHandle = uimenu(hFig,'Label','Display options'); 
 
-        frh = uimenu(menuHandle ,'Label','Histogram max ...');                 
-        uimenu(frh,'Label','range*100%%',  'Callback', {@histogramRangeCallBackFunction, 100, lightingIndex});
-        uimenu(frh,'Label','range*75%%',   'Callback', {@histogramRangeCallBackFunction, 75, lightingIndex});
-        uimenu(frh,'Label','range*50%%',   'Callback', {@histogramRangeCallBackFunction, 50, lightingIndex});
-        uimenu(frh,'Label','range*25%%',   'Callback', {@histogramRangeCallBackFunction, 25, lightingIndex});
-        uimenu(frh,'Label','range*10%%',   'Callback', {@histogramRangeCallBackFunction, 10, lightingIndex});
-        uimenu(frh,'Label','range*5%%',   'Callback', {@histogramRangeCallBackFunction, 5, lightingIndex});
-        uimenu(frh,'Label','range*1%%',   'Callback', {@histogramRangeCallBackFunction, 1, lightingIndex});
+        frh = uimenu(menuHandle ,'Label','Histogram max (X)...'); 
+        uimenu(frh,'Label','100%',  'Callback', {@histogramHorizRangeCallBackFunction, lumRange(2), lightingIndex});
+        uimenu(frh,'Label','50%',   'Callback', {@histogramHorizRangeCallBackFunction, lumRange(2)/2, lightingIndex});
+        uimenu(frh,'Label','25%',   'Callback', {@histogramHorizRangeCallBackFunction, lumRange(2)/4, lightingIndex});
+        uimenu(frh,'Label','10%',   'Callback', {@histogramHorizRangeCallBackFunction, lumRange(2)/10, lightingIndex});
+        
+        frh = uimenu(menuHandle ,'Label','Histogram max (Y)...');                 
+        uimenu(frh,'Label','100%',  'Callback', {@histogramRangeCallBackFunction, 100.0, lightingIndex});
+        uimenu(frh,'Label','75%',   'Callback', {@histogramRangeCallBackFunction, 75.0, lightingIndex});
+        uimenu(frh,'Label','50%',   'Callback', {@histogramRangeCallBackFunction, 50.0, lightingIndex});
+        uimenu(frh,'Label','25%',   'Callback', {@histogramRangeCallBackFunction, 25.0, lightingIndex});
+        uimenu(frh,'Label','10%',   'Callback', {@histogramRangeCallBackFunction, 10.0, lightingIndex});
+        uimenu(frh,'Label','5%',    'Callback', {@histogramRangeCallBackFunction, 5.0, lightingIndex});
+        uimenu(frh,'Label','1%',    'Callback', {@histogramRangeCallBackFunction, 1.0, lightingIndex});
+        uimenu(frh,'Label','0.5%',  'Callback', {@histogramRangeCallBackFunction, 0.5, lightingIndex});
+        uimenu(frh,'Label','0.1%',  'Callback', {@histogramRangeCallBackFunction, 0.1, lightingIndex});
+        
+        
         
         frh = uimenu(menuHandle ,'Label','Data cursor ...');   
-        uimenu(frh,'Label','ON', 'Callback', {@dataCursorCallBackFunction, true, hFig});
-        uimenu(frh,'Label','OFF', 'Callback', {@dataCursorCallBackFunction, false, hFig});
+        uimenu(frh,'Label','ON', 'Callback', {@dataCursorCallBackFunction, true, hFig, 'histogram'});
+        uimenu(frh,'Label','OFF', 'Callback', {@dataCursorCallBackFunction, false, hFig, 'histogram'});
         
         % Generate a tab group
         tabGroupHandle = uitabgroup('Parent', hFig);
@@ -211,7 +298,8 @@ function generateGUI(lumRange, lightingIndex, dataType)
                         set(gca, 'Color', 'k', 'XColor', [0.2 0.9 0.8], 'YColor', [0.2 0.9 0.8], 'YScale', 'linear', 'YLim', YLims, 'XLim', lumRange, 'XTick', [0:500:lumRange(2)], 'YTick', [0:1000:maxHistCount(lightingIndex)], 'YTickLabel', {}, 'XTickLabel', {});
                     end
                     cRatio = max(lumMap(:))/min(lumMap(:));
-                    text(double(luminanceEdges(round(numel(luminanceEdges)*0.77))), maxHistCount(lightingIndex)*0.01, sprintf('LR=%2.1f', cRatio), 'FontSize', 14, 'FontName', 'system', 'Color',  [0.2 0.99 0.8]);
+                    text(0.77,0.9,sprintf('LR=%2.1f', cRatio),'Units','normalized', 'FontSize', 14, 'FontName', 'system', 'Color',  [0.2 0.99 0.8]);
+                    %text(double(luminanceEdges(round(numel(luminanceEdges)*0.77))), maxHistCount(lightingIndex)*0.015, sprintf('LR=%2.1f', cRatio), 'FontSize', 14, 'FontName', 'system', 'Color',  [0.2 0.99 0.8]);
                     box off; grid on;
                     drawnow;
                 end
@@ -228,18 +316,28 @@ function generateGUI(lumRange, lightingIndex, dataType)
 end
 
 
-function dataCursorCallBackFunction(src, eventdata, status, hFig)
+function dataCursorCallBackFunction(src, eventdata, status, hFig, dataType)
 
     dcm_obj = datacursormode(hFig);
     
     if (status)
         set(dcm_obj, 'Enable', 'on');
         set(dcm_obj, 'DisplayStyle', 'datatip');
-       %set(dcm_obj, 'DisplayStyle', 'window');
+        
+        if strcmp(dataType, 'histogram')
+            set(dcm_obj, 'updatefcn', @dataTipForHistogramData);
+        end
     else
         set(dcm_obj, 'Enable', 'off');
+        dcm_obj.removeAllDataCursors();
+    end
+    
+    function outputText = dataTipForHistogramData(obj, event_obj)
+        pos = get(event_obj, 'Position');
+        outputText = {sprintf('Luminance: %s', num2str(pos(1), '%4.1f')), sprintf('Pixels no: %s', num2str(pos(2), '%d'))};
     end
 end
+
 
 function fullLumRangeCallBackFunction(src, eventdata, lightingCondIndex)
 
@@ -287,7 +385,7 @@ function histogramRangeCallBackFunction(src, eventdata, p, lightingCondIndex)
     
     YLims = [0 maxHistCount(lightingCondIndex)*p/100];
     
-    hWaitBar = waitbar(0,sprintf('Updating CLim for lighting condition: %d', lightingCondIndex));
+    hWaitBar = waitbar(0,sprintf('Updating histogram YLim for lighting condition: %d', lightingCondIndex));
     pause(0.01);
     steps = numel(shapeConds)*numel(specularSPDconds)*numel(alphaConds);
 
@@ -308,8 +406,44 @@ function histogramRangeCallBackFunction(src, eventdata, p, lightingCondIndex)
     end
 
     close(hWaitBar);
-    
 end
+
+
+function histogramHorizRangeCallBackFunction(src, eventdata, maxLum, lightingCondIndex)
+    global shapeConds
+    global alphaConds
+    global specularSPDconds
+    global lightingConds
+    global luminanceMaps
+    global imageDisplayHandles
+
+    hWaitBar = waitbar(0,sprintf('Updating histogram XLim for lighting condition: %d', lightingCondIndex));
+    pause(0.01);
+    steps = numel(shapeConds)*numel(specularSPDconds)*numel(alphaConds);
+
+  
+    stepNo = 0;
+    for shapeIndex = 1:numel(shapeConds)
+        for specularSPDindex = 1:numel(specularSPDconds)
+            for alphaIndex = 1:numel(alphaConds)
+                stepNo = stepNo + 1;
+                waitbar(stepNo / steps, hWaitBar );
+                pause(0.01);
+                plotAxesHandle = imageDisplayHandles(lightingCondIndex,shapeIndex,alphaIndex, specularSPDindex).histogramPlotHandle;
+                subplot(plotAxesHandle);
+                XLims = get(plotAxesHandle, 'XLim');
+                XLims(2) = maxLum;
+                set(plotAxesHandle, 'XLim', XLims);
+                drawnow;
+            end
+        end
+    end
+
+    close(hWaitBar);
+end
+
+
+
 
 function percentileLumRangeCallBackFunction(src, eventdata, p, lightingCondIndex)
 
