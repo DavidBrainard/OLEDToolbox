80d6a7b2c69adc28224f313350801948ff9374cf
np_cottaris <cottaris@sas.upenn.edu>
2015-05-09 12:57:55 -0400

    updated conversion script

--- ToneMappingApp/RT3sceneConversion/ConvertRT3scene.m
+++ ToneMappingApp/RT3sceneConversion/ConvertRT3scene.m
@@ -1,38 +1,41 @@
 function ConvertRT3scene
 
-   blobbieMultiSpectralDataDir = fullfile(OLEDToolboxRootPath,'HDRstuff', 'BlobbieAnalysis','MultispectralData_0deg');
-   [blobbieSceneFileName, blobbieMultiSpectralDataDir] = uigetfile({'*.mat'},'Select a .mat file', blobbieMultiSpectralDataDir);
-   if (blobbieMultiSpectralDataDir==0)
+    blobbieMultiSpectralDataDir = fullfile(OLEDToolboxRootPath,'HDRstuff', 'BlobbieAnalysis','MultispectralData_0deg');
+    [blobbieSceneFileName, blobbieMultiSpectralDataDir] = uigetfile({'*.mat'},'Select a .mat file', blobbieMultiSpectralDataDir);
+    if (blobbieMultiSpectralDataDir==0)
        return;
-   end
-   load(fullfile(blobbieMultiSpectralDataDir, blobbieSceneFileName), 'S', 'multispectralImage');
-   
-   % compute sensorXYZ image
-   sensorXYZ = loadXYZCMFs();
-   
-   % multispectral to XYZ
-   XYZimage = MultispectralToSensorImage(multispectralImage, S, sensorXYZ.T, sensorXYZ.S);
-   
-   % to cal format
-   [XYZcalFormat, nCols, mRows] = ImageToCalFormat(XYZimage);
-   
-   % to linear sRGB
-   linearSRGBcalFormat = XYZToSRGBPrimary(XYZcalFormat);
-   
-   % to gamma-corrected sRGB
-   gammaCorrectedSRGBcalFormat = sRGB.gammaCorrect(linearSRGBcalFormat);
-            
-   % to image format
-   gammaCorrectedSRGBimage = CalFormatToImage(gammaCorrectedSRGBcalFormat, nCols, mRows);
-   
-   % save gamma corrected SRGB image
-   SRGBImageDestinationDir = fullfile(OLEDToolboxRootPath,'ToneMappingApp', 'SRGBimages');
-   
-   % The tone mapping app, expects the gamma corrected SRGBimage in a variable named 'inputSRGBimage'
-   inputSRGBimage = gammaCorrectedSRGBimage;
-   filename = fullfile(SRGBImageDestinationDir, blobbieSceneFileName);
-   save(filename, 'inputSRGBimage');
-   fprintf('Gamma corrected SRGB image saved in the ''%ss''.', filename);
+    end
+    load(fullfile(blobbieMultiSpectralDataDir, blobbieSceneFileName), 'S', 'multispectralImage');
+
+    % compute sensorXYZ image
+    sensorXYZ = loadXYZCMFs();
+
+    % multispectral to XYZ
+    XYZimage = MultispectralToSensorImage(multispectralImage, S, sensorXYZ.T, sensorXYZ.S);
+
+    % to cal format
+    [XYZcalFormat, nCols, mRows] = ImageToCalFormat(XYZimage);
+
+    % to linear sRGB
+    linearSRGBcalFormat = XYZToSRGBPrimary(XYZcalFormat);
+    
+    % linear sRGBimage
+    linearSRGBimage = CalFormatToImage(linearSRGBcalFormat, nCols, mRows);
+    
+    saveInGammaCorrectedSRGBformat = false;
+    if (saveInGammaCorrectedSRGBformat)
+        % to gamma-corrected sRGB
+        gammaCorrectedSRGBcalFormat = sRGB.gammaCorrect(linearSRGBcalFormat);
+
+        % to image format
+        gammaCorrectedSRGBimage = CalFormatToImage(gammaCorrectedSRGBcalFormat, nCols, mRows);
+    end
+    
+    % save image data
+    SRGBImageDestinationDir = fullfile(OLEDToolboxRootPath,'ToneMappingApp', 'SRGBimages');
+    filename = fullfile(SRGBImageDestinationDir, blobbieSceneFileName);
+    save(filename, 'linearSRGBimage');
+    fprintf('Linear SRGB image saved in ''%ss''.', filename);
 end
 
 function sensorXYZ = loadXYZCMFs()
