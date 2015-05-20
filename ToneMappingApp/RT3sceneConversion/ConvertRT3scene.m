e4468e474be53d873ea875f017cc2be20985c63d
np_cottaris <cottaris@sas.upenn.edu>
2015-05-20 17:08:57 -0400

    Improvements in RT3 conversions
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- ToneMappingApp/RT3sceneConversion/ConvertRT3scene.m
+++ ToneMappingApp/RT3sceneConversion/ConvertRT3scene.m
@@ -1,16 +1,25 @@
-function ConvertRT3scene
+% Function to return a linearSRGB version of a multispectral Blobbie image
+% that is graded (scaled) so that the white square of a MacBeth color checker 
+% that is placed at the center of the scene and at a 45 angle  would have 
+% a mean sRGB value of 0.5
+function returnedData = ConvertRT3scene(varargin)
 
-    blobbieMultiSpectralDataDir = fullfile(OLEDToolboxRootPath,'HDRstuff', 'BlobbieAnalysis','MultispectralData_0deg');
-    [blobbieSceneFileName, blobbieMultiSpectralDataDir] = uigetfile({'*.mat'},'Select a .mat file', blobbieMultiSpectralDataDir);
-    if (blobbieMultiSpectralDataDir==0)
-       return;
+    if (nargin == 0)
+        blobbieMultiSpectralDataDir = fullfile(OLEDToolboxRootPath,'HDRstuff', 'BlobbieAnalysis','MultispectralData_0deg');
+        [blobbieSceneFileName, blobbieMultiSpectralDataDir] = uigetfile({'*.mat'},'Select a .mat file', blobbieMultiSpectralDataDir);
+        if (blobbieMultiSpectralDataDir==0)
+           return;
+        end
+    else
+        blobbieMultiSpectralDataDir = varargin{1};
+        blobbieSceneFileName = varargin{2};
     end
     
-    % Grade image according to factors extracted by the script
+    % Select scaling factor based on lighting (as extracted from filename)
+    % The scaling factors below were extracted by the script
     % /Users/Shared/Matlab/Toolboxes/OLEDToolbox/ToneMappingApp/RT3sceneConversion/GradeBlobbiesBySettingsDesiredPatchMeanSRGB.m
     % with colorCheckerOrientation = 'TILTED'; 
-    % targetPatch = the white color check and 
-    % targetSRGB = 0.5
+    % targetPatch = the white color check and  targetSRGB = 0.5
     if (strfind(blobbieSceneFileName, 'area1_front0_ceiling0'))
         scalingFactor = 0.1055343;
     elseif (strfind(blobbieSceneFileName, 'area0_front0_ceiling1'))
@@ -18,14 +27,14 @@ function ConvertRT3scene
     else
         error('No scaling factor for image %s', blobbieSceneFileName);
     end
-    
+        
+        
+    % load multispectral image data
+    load(fullfile(blobbieMultiSpectralDataDir, blobbieSceneFileName), 'S', 'multispectralImage');
     
     % load XYZ CMFs
     sensorXYZ = loadXYZCMFs();
 
-    % load multispectral image data
-    load(fullfile(blobbieMultiSpectralDataDir, blobbieSceneFileName), 'S', 'multispectralImage');
-    
     % compute XYZimage
     XYZimage = MultispectralToSensorImage(multispectralImage, S, sensorXYZ.T, sensorXYZ.S);
 
@@ -43,21 +52,28 @@ function ConvertRT3scene
     % no negative sRGB values
     linearSRGBimage(linearSRGBimage<0) = 0;
     
-    saveInGammaCorrectedSRGBformat = false;
-    if (saveInGammaCorrectedSRGBformat)
-        % to gamma-corrected sRGB
-        gammaCorrectedSRGBcalFormat = sRGB.gammaCorrect(linearSRGBcalFormat);
+    
+    if (nargin == 0)
+        saveInGammaCorrectedSRGBformat = false;
+        if (saveInGammaCorrectedSRGBformat)
+            % to gamma-corrected sRGB
+            gammaCorrectedSRGBcalFormat = sRGB.gammaCorrect(linearSRGBcalFormat);
 
-        % to image format
-        gammaCorrectedSRGBimage = CalFormatToImage(gammaCorrectedSRGBcalFormat, nCols, mRows);
+            % to image format
+            gammaCorrectedSRGBimage = CalFormatToImage(gammaCorrectedSRGBcalFormat, nCols, mRows);
+        end
+
+
+        % save image data
+        SRGBImageDestinationDir = fullfile(OLEDToolboxRootPath,'ToneMappingApp', 'SRGBimages');
+        filename = fullfile(SRGBImageDestinationDir, blobbieSceneFileName);
+        save(filename, 'linearSRGBimage');
+        fprintf('Linear SRGB image saved in ''%ss''.', filename);
+
+        PlotSRGBImages(1, 'Original and Graded images', linearSRGBimage, linearSRGBimage/scalingFactor, 'Graded image', 'Original image', []);
+        returnedData = [];
+    else
+        returnedData = linearSRGBimage;
     end
-    
-    % save image data
-    SRGBImageDestinationDir = fullfile(OLEDToolboxRootPath,'ToneMappingApp', 'SRGBimages');
-    filename = fullfile(SRGBImageDestinationDir, blobbieSceneFileName);
-    save(filename, 'linearSRGBimage');
-    fprintf('Linear SRGB image saved in ''%ss''.', filename);
-    
-    PlotSRGBImages(1, 'Original and Graded images', linearSRGBimage, linearSRGBimage/scalingFactor, 'Graded image', 'Original image', []);
     
 end
