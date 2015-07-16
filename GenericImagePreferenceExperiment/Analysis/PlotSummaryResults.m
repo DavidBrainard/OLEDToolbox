abeaaa4394d9280d56c7308ece4fad0678d921e0
np_cottaris <cottaris@sas.upenn.edu>
2015-07-16 16:33:45 -0400

    improvements
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- GenericImagePreferenceExperiment/Analysis/PlotSummaryResults.m
+++ GenericImagePreferenceExperiment/Analysis/PlotSummaryResults.m
@@ -15,7 +15,7 @@ function PlotSummaryResults
     vector(2,:) = [linearFit.x(end) linearFit.y(end)];
     
     for k = 1:numel(alphaHDRdavid)
-        pointQ(:,k) = [alphaHDRdavid(k);alphaLDRdavid(k)];
+        pointQ(:,k) = [alphaHDRdavid(k); alphaLDRdavid(k)];
         [pointQproj(:,k), length_q] = ProjectPointToVector(vector, pointQ(:,k));
     end
     
@@ -28,11 +28,15 @@ function PlotSummaryResults
     plot(dynamicRange, alphaLDRnicolas, 'ks-','MarkerSize', 12, 'MarkerFaceColor', [1 0.8 0.8], 'MarkerEdgeColor', [0 0 0]);
     plot(dynamicRange, alphaHDRdavid, 'ko-','MarkerSize', 12, 'MarkerFaceColor', [0.8 0.8 1.0], 'MarkerEdgeColor', [0 0 0 ]);
     plot(dynamicRange, alphaLDRdavid, 'ks-','MarkerSize', 12, 'MarkerFaceColor', [0.8 0.8 1.0], 'MarkerEdgeColor', [0 0 0]);
+    
+    plot(dynamicRange, pointQproj(1,:), 'ko-','MarkerSize', 10, 'MarkerFaceColor', 'c', 'MarkerEdgeColor', 'b');
+    plot(dynamicRange, pointQproj(2,:), 'ks-','MarkerSize', 10, 'MarkerFaceColor', 'c', 'MarkerEdgeColor', 'b');
+    
     set(gca, 'FontSize', 14);
     set(gca, 'YLim', [1 220]);
-    legend('HDRnicolas', 'LDRnicolas', 'HDRdavid', 'LDRdavid');
+    legend('HDRnicolas', 'LDRnicolas', 'HDRdavid', 'LDRdavid', 'HDRdavid-projections', 'LDRdavid-projections');
     grid on
-    xlabel('scene ynamic range', 'FontSize', 14, 'FontWeight', 'bold');
+    xlabel('scene dynamic range', 'FontSize', 14, 'FontWeight', 'bold');
     ylabel('best alpha',  'FontSize', 14, 'FontWeight', 'bold');
     
     subplot(1,2,2);
@@ -52,6 +56,9 @@ function PlotSummaryResults
     ylabel('LDR alpha',  'FontSize', 14, 'FontWeight', 'bold');
     grid on
     set(gca, 'FontSize', 14);
+    
+    
+    
     drawnow
     NicePlot.exportFigToPDF(pdfFileName, h, 300);
     
@@ -59,7 +66,7 @@ end
 
 function [dynamicRange, alphaHDR, alphaLDR, pdfFileName] = GetDHBData()
     dynamicRange = [54848 2806 15814 1079 19581 2789 6061 1110];
-    [s,ix] = sort(dynamicRange)
+    [s,ix] = sort(dynamicRange);
     alphaHDR = [40.1 39.6 55.1 56.4 56.9 57.7 60.7 65.0];
     alphaLDR = [108.5 92.3 217 217 124.7 104.6 124.7 155.5];
     dynamicRange = dynamicRange(ix);
@@ -70,8 +77,12 @@ end
 
 function [dynamicRange, alphaHDR, alphaLDR, pdfFileName] = GetNicolasData()
     dynamicRange = [54848 2806 15814 1079 19581 2789 6061 1110];
+    [s,ix] = sort(dynamicRange);
     alphaHDR = [12.9 19.7 16.3 27.4 16.7 23.6 21.5 35];
     alphaLDR = [23.3 33.3 31.5 54.6 30.7 47.3 35.3  65.8];
+    dynamicRange = dynamicRange(ix);
+    alphaHDR = alphaHDR(ix);
+    alphaLDR = alphaLDR(ix);
     pdfFileName = 'SummaryAlphasNicolas.pdf';
 end
 
