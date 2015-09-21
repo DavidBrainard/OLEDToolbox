01545df868629b1193691f250f481c6e52ee878c
np_cottaris <cottaris@sas.upenn.edu>
2015-09-21 14:59:01 -0400

    Updates
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- GenericImagePreferenceExperiment/RunImagePreferenceExperiment.m
+++ GenericImagePreferenceExperiment/RunImagePreferenceExperiment.m
@@ -5,12 +5,16 @@ function RunImagePreferenceExperiment
     cd(rootDir);
 
     
-    % Select a stimulus cache file. This will determine the experiment to
-    % be run.
+    % Select a stimulus cache file. This will determine the experiment to be run.
+    
+    % Universal for all subjects. This is to map their optimal LCD and OLED
+    % tone mapping functions
     cacheFileNameList = {...
         fullfile(rootDir,'Caches', 'Blobbie_SunRoomSideLight_Reinhardt_Cache.mat') ...
         };
     
+    % Individual subject cache files that contain their optimal LCD/OLED tone
+    % mapping as determined by experiment 1
     cacheFileNameList = {...
         fullfile(rootDir,'Caches', 'Blobbie_SunRoomSideLight_Cache_HDR_vs_optimalLDR_David.mat') ...
         };
@@ -21,8 +25,9 @@ function RunImagePreferenceExperiment
     
         
     fprintf('\n----------------------------------------------------------------------------');
-    fprintf('\n\nStimulus cache to load: <strong> %s </strong>', cacheFileNameList{1});
-    fprintf('\nIf this is the desired cache file, hit enter to continue ... ');
+    fprintf(2,'\n\nPlease make sure that this is the right cache file.\n');
+    fprintf('<strong> %s </strong>', cacheFileNameList{1});
+    fprintf('\nIf this is the desired cache file, hit enter to continue. Otherwise ^c to exit ... ');
     pause
     fprintf('\n----------------------------------------------------------------------------\n');
     
