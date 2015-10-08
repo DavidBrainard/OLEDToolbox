56215c78e1ead37e11f07b5808c3a2ff9d378d5d
Nicolas Cottaris <cottaris@sas.upenn.edu>
2015-10-08 14:32:26 -0400

    Better selection of cache files

--- GenericImagePreferenceExperiment/RunImagePreferenceExperiment.m
+++ GenericImagePreferenceExperiment/RunImagePreferenceExperiment.m
@@ -5,13 +5,27 @@ function RunImagePreferenceExperiment
     cd(rootDir);
 
     
-    % Select a stimulus cache file. This will determine the experiment to be run.
     
-    % Universal for all subjects. This is to map their optimal LCD and OLED
-    % tone mapping functions
-    cacheFileNameList = {...
-        fullfile(rootDir,'Caches', 'Blobbie_SunRoomSideLight_Reinhardt_Cache.mat') ...
-        };
+    % Select a stimulus cache file. This will determine the experiment to be run.
+    if (strcmp(whichDisplay, 'HDR')) || (strcmp(whichDisplay, 'LDR'))
+        % Universal for all subjects. This is to map their optimal LCD and OLED
+        % tone mapping functions
+        cacheFileNameList = {...
+            fullfile(rootDir,'Caches', 'Blobbie_SunRoomSideLight_Reinhardt_Cache.mat') ...
+            };
+    elseif (strcmp(whichDisplay, 'fixOptimalLDR_varyHDR'))
+        subjectName = input('Enter subject name: ', 's');
+        cacheFileNameList = {...
+            sprintf('%s_%s.mat',fullfile(rootDir,'Caches', 'Blobbie_SunRoomSideLight_Cache_HDR_vs_optimalLDR'), subjectName) ...
+         };
+    else
+        error('Unknown experiment %s', whichDisplay);
+    end
+    
+    if (~exist(cacheFileNameList{1}, 'file'))
+        fprintf('Cache file ''%s'' not found on the path. Exiting ...', cacheFileNameList{1});
+        return;
+    end
     
     % Individual subject cache files that contain their optimal LCD/OLED tone
     % mapping as determined by experiment 1
@@ -25,7 +39,7 @@ function RunImagePreferenceExperiment
 %     
         
     fprintf('\n----------------------------------------------------------------------------');
-    fprintf(2,'\n\nPlease make sure that this is the right cache file.\n');
+    fprintf(2,'\n\nPlease make sure that this is the desired cache file.\n');
     fprintf('<strong> %s </strong>', cacheFileNameList{1});
     fprintf('\nIf this is the desired cache file, hit enter to continue. Otherwise ^c to exit ... ');
     pause
